2471b7e0b2b9a125010ac13bea7696a6190940fd
Ali Jiryaei <jiryaei2014@gmail.com>
2023-10-27 22:41:30 +0330

    fix: fixed price formatter

--- utils/Helper.ts
+++ utils/Helper.ts
@@ -4,11 +4,10 @@ const USDollar = new Intl.NumberFormat("en-US");
 
 const priceFormatter = (price: string | number): string => {
   const newPrice = Number(price);
-  const fixedNumber =
-    newPrice > 0.01 ? newPrice.toFixed(2) : newPrice.toFixed(7);
+  const formattedPrice =
+    newPrice > 0.01 ? newPrice.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") : newPrice.toFixed(7);
 
-  return `$${USDollar.format(+fixedNumber)}`;
+  return `$${formattedPrice}`;
 };
-
 
 export { priceFormatter };
